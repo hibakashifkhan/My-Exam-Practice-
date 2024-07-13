c642a4063f1c5386d23a297fb762c5442a3e8776
unknown <hibakashifkhan1@gmail.com>
2024-07-13 19:29:46 +0500

    Adding My TypeScript Practice

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+// Write a program that checks if a number is even or odd.
+
+
+var number = 10;
+if (number % 2 === 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
+
+
+
+
